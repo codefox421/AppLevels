9cbc003b11325382d114888bf0d03fc11632a4e3
Nick Iaconis <nick.iaconis+github@gmail.com>
2013-02-23 02:47:05 -0500

    Refactored MainActivity to AppLevelsMain
    Reason: AppLevelsMain is a marginally more descriptive name

--- src/com/codefox421/applevels/AppLevelsMain.java
+++ src/com/codefox421/applevels/AppLevelsMain.java
@@ -1,3 +1,26 @@
+/**
+ * Copyright 2013 Nick Iaconis
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ * Filename:	AppLevelsMain.java
+ * Class:		AppLevelsMain
+ * 
+ * Purpose:		As the user-facing portion of AppLevels, AppLevelsMain is
+ * 				responsible for managing the GUI and the operation of the
+ * 				background helper service, AppLevelsService.
+ */
+
 package com.codefox421.applevels;
 
 import java.util.ArrayList;
@@ -21,7 +44,7 @@ import android.widget.CompoundButton;
 import android.widget.ListView;
 import android.widget.ProgressBar;
 
-public class MainActivity extends Activity {
+public class AppLevelsMain extends Activity {
 	
 	CompoundButton toggleButton;
 	ListView managedAppsList;
@@ -89,10 +112,10 @@ public class MainActivity extends Activity {
     public void toggleService(View view) {
     	if(isServiceRunning()) {
     		Log.d("AppLevels", "Stopping Service...");
-    		stopService(new Intent(MainActivity.this, AppLevelsService.class));
+    		stopService(new Intent(AppLevelsMain.this, AppLevelsService.class));
     	} else {
     		Log.d("AppLevels", "Starting Service...");
-    		startService(new Intent(MainActivity.this, AppLevelsService.class));
+    		startService(new Intent(AppLevelsMain.this, AppLevelsService.class));
     	}
     	
     	toggleButton.setChecked(isServiceRunning());
